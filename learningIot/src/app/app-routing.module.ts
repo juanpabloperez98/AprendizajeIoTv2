8541469e698d1253dc1d9d492e46014fa4b4a545
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-09-14 16:07:55 -0500

    feat: microcontroller module created

--- learningIot/src/app/app-routing.module.ts
+++ learningIot/src/app/app-routing.module.ts
@@ -1,69 +1,91 @@
-import { Routes } from "@angular/router";
-import { HomeComponent } from "./components/home/home.component"
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 export const AppRoutes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'introduccion',
-    children:[
+    children: [
       {
-        path:'',
-        loadChildren: () => import('./modules/introduction/introduction.module').then( m => m.IntroductionModule)
-      }
-    ]
+        path: '',
+        loadChildren: () =>
+          import('./modules/introduction/introduction.module').then(
+            (m) => m.IntroductionModule
+          ),
+      },
+    ],
   },
   {
     path: 'presentationOne',
-    children:[
+    children: [
       {
-        path:'',
-        loadChildren: () => import('./modules/presentation1/presentation1.module').then( m => m.Presentation1Module)
-      }
-    ]
+        path: '',
+        loadChildren: () =>
+          import('./modules/presentation1/presentation1.module').then(
+            (m) => m.Presentation1Module
+          ),
+      },
+    ],
   },
   {
     path: 'presentationTwo',
-    children:[
+    children: [
       {
-        path:'',
-        loadChildren: () => import('./modules/presentation2/presentation2.module').then( m => m.Presentation2Module)
-      }
-    ]
+        path: '',
+        loadChildren: () =>
+          import('./modules/presentation2/presentation2.module').then(
+            (m) => m.Presentation2Module
+          ),
+      },
+    ],
   },
   {
     path: 'aplicaciones',
-    children:[
+    children: [
       {
-        path:'',
-        loadChildren: () => import('./modules/aplicaciones/aplicaciones.module').then( m => m.AplicacionesModule)
-      }
-    ]
+        path: '',
+        loadChildren: () =>
+          import('./modules/aplicaciones/aplicaciones.module').then(
+            (m) => m.AplicacionesModule
+          ),
+      },
+    ],
   },
   {
     path: 'redes',
-    children:[
+    children: [
       {
-        path:'',
-        loadChildren: () => import('./modules/redes/redes.module').then( m => m.RedesModule)
-      }]
-    },
-    {
-      path: 'fundamentos',
-      children:[
+        path: '',
+        loadChildren: () =>
+          import('./modules/redes/redes.module').then((m) => m.RedesModule),
+      },
+    ],
+  },
+  {
+    path: 'fundamentos',
+    children: [
       {
-        path:'',
-        loadChildren: () => import('./modules/fundamentos-electronica/fundamentos-electronica.module').then( m => m.FundamentosElectronicaModule)
-      }
-    ]
+        path: '',
+        loadChildren: () =>
+          import(
+            './modules/fundamentos-electronica/fundamentos-electronica.module'
+          ).then((m) => m.FundamentosElectronicaModule),
+      },
+    ],
+  },
+  {
+    path: 'microcontroller',
+    loadChildren: () =>
+      import('./modules/microcontroller/microcontroller.module').then(
+        (m) => m.MicrocontrollerModule
+      ),
   },
   {
     path: '**',
-    redirectTo:'',
+    redirectTo: '',
   },
 ];
-
-
---
+++ learningIot/src/app/modules/microcontroller/microcontroller.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+
+export const microcontrollerRoutes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+];
