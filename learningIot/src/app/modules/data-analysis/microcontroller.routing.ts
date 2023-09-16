511e68c9b4140999e7036a1aaa37bd4cbcd382bf
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-09-15 20:02:11 -0500

    feat: data-analysis module created

---
+++ learningIot/src/app/modules/data-analysis/microcontroller.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+
+export const dataAnalysisRoutes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+];
