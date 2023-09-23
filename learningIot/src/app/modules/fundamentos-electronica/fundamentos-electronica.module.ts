59c33472acc2c15deebcc0047c73d4e7476aaa40
juan pablo peres <juanpabloperezdevelopment@gmail.com>
2023-09-23 14:17:57 -0500

    adding questions 2 module

--- learningIot/src/app/modules/fundamentos-electronica/fundamentos-electronica.module.ts
+++ learningIot/src/app/modules/fundamentos-electronica/fundamentos-electronica.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { fundamentosElectronicaRoutingModule } from './fundamentos-electronica-routing.module';
+import { fundamentosElectronicaRoutingModule } from './fundamentos-electronica.routing';
+import { QuestionsComponent } from './questions/questions.component';
 
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    QuestionsComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     fundamentosElectronicaRoutingModule,
+    FormsModule,
   ]
 })
 export class FundamentosElectronicaModule { }
