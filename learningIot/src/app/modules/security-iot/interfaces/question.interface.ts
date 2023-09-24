d4bf521222f73db6bd0f3a1e11a638be6e47f289
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-09-23 21:37:57 -0500

    feat: changes question security completed

---
+++ learningIot/src/app/modules/security-iot/interfaces/question.interface.ts
@@ -0,0 +1,9 @@
+export interface OrderList {
+  question: string[];
+  response: string[];
+}
+
+export interface ListDragSecurity {
+  title: string;
+  orderList: OrderList;
+}
--- learningIot/src/app/modules/security-iot/security-iot.routing.ts
+++ learningIot/src/app/modules/security-iot/security-iot.routing.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './components/main/main.component';
+import { SecurityQuestionComponent } from './components/security-question/security-question.component';
 
 export const securityIotRoutes: Routes = [
   {
     path: '',
     component: MainComponent,
+  },
+  {
+    path: 'questions',
+    component: SecurityQuestionComponent,
   },
 ];
